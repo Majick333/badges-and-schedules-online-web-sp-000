a1e60a76718c003704821aaaf02730554e8b8ca2
Ryan M <rmccauleymusic@gmail.com>
2019-10-26 18:04:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,47 @@
-# Write your code here.
+ # Write your code here.
+
+# let(:name) {"Arel"}
+ #let(:attendees) {["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]}
+ #let(:badges) {[
+                 # "Hello, my name is Edsger.",
+                 # "Hello, my name is Ada.",
+                 # "Hello, my name is Charles.",
+                 # "Hello, my name is Alan.",
+                 # "Hello, my name is Grace.",
+                 # "Hello, my name is Linus.",
+                 # "Hello, my name is Matz."
+
+name = []
+attendees = []
+badges = []
+room = []
+
+ def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(attendees)
+  badges = []
+    attendees.each do |attendee|
+      puts  badges.push(badge_maker(attendee))
+    end
+  return badges
+end
+
+
+def assign_rooms(attendees)
+  room_assignments = []
+  attendees.each_with_index do |attendee, index|
+    puts room_assignments.push("Hello, #{attendee}! You'll be assigned to room #{index + 1}!")
+  end
+  return room_assignments
+end
+
+def printer(attendees)
+  ans = []
+  attendees.each do |attendee|
+puts ans.push (batch_badge_creator(attendees) && assign_rooms(attendees))
+end
+  puts ans
+end 
